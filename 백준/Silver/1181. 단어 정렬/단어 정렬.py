5517b8a37c39bb19f9ce1afc7c464266b798f948
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2023-07-08 20:39:26 +0900

    [Silver V] Title: 단어 정렬, Time: 3484 ms, Memory: 33300 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = []
+
+for i in range(n):
+    a = input()
+    if a not in lst:
+        lst.append(a)
+
+lst.sort()
+lst.sort(key=len)
+
+for i in lst:
+    print(i,end='')
