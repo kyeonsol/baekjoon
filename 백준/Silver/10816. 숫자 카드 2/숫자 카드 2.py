9115b4a05fedee9ea83b6529e5017d4c4b199e44
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2023-07-24 17:25:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 976 ms, Memory: 113796 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = sorted(list(map(int,input().split())))
+
+m = int(input())
+lst2 = list(map(int,input().split()))
+cnt = {}
+
+for i in lst:
+    if i in cnt:
+        cnt[i] += 1
+    else:
+        cnt[i] = 1
+
+for i in lst2:
+    if i in cnt:
+        print(cnt[i],end=' ')
+    else:
+        print(0, end=' ')
