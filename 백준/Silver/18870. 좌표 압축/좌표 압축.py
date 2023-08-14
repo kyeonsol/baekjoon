15c42d839dab733a217bec9cadb94635d564a1e4
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2023-08-14 22:13:56 +0900

    [Silver II] Title: 좌표 압축, Time: 2000 ms, Memory: 154804 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int,input().split()))
+
+lst_s = sorted(set(lst))
+
+dic = dict()
+cnt = 0
+
+for i in lst_s:
+    dic[i] = cnt
+    cnt += 1
+
+for n in lst:
+    print(dic[n],end=' ')
