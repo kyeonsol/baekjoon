e68b344be0645ee793aa12dc4263da47f6025209
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2023-11-24 15:46:42 +0900

    [Bronze I] Title: 2007년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+a = [31,28,31,30,31,30,31,31,30,31,30,31]
+week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+day = 0
+
+x, y = map(int,input().split())
+
+for i in range(0,x-1):
+    day += a[i]
+
+day = (day + y) % 7
+
+print(week[day])
