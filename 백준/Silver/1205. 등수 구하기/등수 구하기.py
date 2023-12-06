4371820b604d3f7bfd7d30b60e9e798d6655b33e
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2023-12-06 22:39:30 +0900

    [Silver IV] Title: 등수 구하기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, new, p = map(int,input().split())
+if n == 0:
+    print(1)
+else:
+    score = list(map(int,input().split()))
+    if n == p and score[-1] >= new:
+        print(-1)
+    else:
+        res = n + 1
+        for i in range(n):
+            if score[i] <= new:
+                res = i + 1
+                break
+        print(res)
