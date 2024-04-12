9f5a05cadef1524d919bbb72c37742d66eb49231
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-04-13 00:57:04 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,9 +1,30 @@
-n= int(input())
-cnt=0
+n = int(input())
+cnt = 0
 
 for i in range(n):
-    a= input()
-    if list(a)== sorted(a,key=a.find):
-        cnt +=1
+    lst = []
+    str = list(input())
+    lst.append(str[0])
+    flag = True
+    for j in range(len(str)-1):
+        if str[j] != str[j+1]:
+            if str[j+1] not in lst:
+                lst.append(str[j+1])
+            else:
+                flag = False
+        else:
+            continue
+    if flag == True:
+        cnt += 1
 
 print(cnt)
+        
+        
+
+            
+
+#전글자와 다음 글자가 다른가?
+#네 : 그렇다면 다음 글자가 이미 나온 글자인가?
+#    네 : 탈락
+#    아니오 : 리스트에 넣고 계속 진행
+#아니오 : 계속 진행
