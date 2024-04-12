ad14a2745f1c8643bf50503b358367267b553412
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-04-13 01:45:14 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,14 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 n,m = map(int,input().split())
-A,B = [],[]
+lst1 = []
+lst2 = []
 
 for i in range(n):
     a = list(map(int,input().split()))
-    A.append(a)
+    lst1.append(a) #리스트 안에 리스트 m개를 생성
 
 for i in range(n):
     b = list(map(int,input().split()))
-    B.append(b)
+    lst2.append(b)
 
 for i in range(n):
+    total = []
     for j in range(m):
-        print(A[i][j] + B[i][j], end=' ')
+        total.append(lst1[i][j] + lst2[i][j])
+    print(*total)
