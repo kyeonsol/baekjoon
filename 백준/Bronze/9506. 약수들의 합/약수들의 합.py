6f9142877d015cd59fb9b7e78140930b0e8b35e0
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-04-26 04:18:46 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,16 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+
+
 while 1:
     n = int(input())
+    lst = [] #n의 약수가 담긴 리스트
     if n == -1:
         break
-    lst = []
-    for i in range(1,n):
-        if n % i == 0:
+
+    for i in range(1,n): #약수구하기
+        if n%i == 0:  
             lst.append(i)
-    if sum(lst) == n:
-        result = ""
-        for i in lst:
-            result += str(i) + " + "
-        result = result[:-3]
-        print(n, '=', result)
+            
+    if sum(lst) == n: #약수의 합이 n과 같으면
+        print(f"{n} = ",end='')
+        print(*lst,sep=' + ')
     else:
-        print(n,"is NOT perfect.")
+        print(f"{n} is NOT perfect.")
