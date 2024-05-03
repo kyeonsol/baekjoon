7f5d8703ce6616b955cd275104da726fee35220b
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-03 15:34:48 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,29 +1,28 @@
-n,m = map(int,input().split())
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split()) #세로, 가로
 lst = []
-error = []
 
 for i in range(n):
-    a = input()
-    lst.append(a)
+    lst.append(input().strip()) #체스판 받기
 
-for i in range(n-7):
-    for j in range(m-7):
-        white = 0
-        black = 0
-        
-        for k in range(i,i+8):  #열
-            for l in range(j,j+8):  #행
-                if (k+l)%2 == 0:  #짝수
-                    if lst[k][l] != 'W':
-                        white += 1
-                    else:
-                        black += 1
-                else:
-                    if lst[k][l] != 'B':
-                        white += 1
-                    else:
-                        black += 1
-        error.append(white)
-        error.append(black)
+res = 64
 
-print(min(error))
+for i in range(0,n-7): #세로
+    for j in range(0,m-7): #가로
+        b_cnt = 0
+        w_cnt = 0
+
+        for x in range(i,i+8): #체스판 생성
+            for y in range(j,j+8):
+
+                if (x+y)%2 == 0 and lst[x][y] == 'W': #하얀색으로 시작 == 좌표를 더한 값이 짝수인 곳이 W여야 함
+                    w_cnt += 1 #옳으면 추가
+                elif (x+y)%2 != 0 and lst[x][y] == 'B': #검은색으로 시작 == 좌표를 더한 값이 홀수인 곳이 B여야 함
+                    b_cnt += 1
+
+        temp = 64- (w_cnt + b_cnt) #틀린 수
+        res = min(res, temp, 64-temp) #전체 - w일 때 옳은 수 == b일 때 틀린 수
+
+print(res)
