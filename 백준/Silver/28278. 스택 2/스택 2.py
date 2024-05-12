bffb45720e112b30225aa86422d7be82bb66a051
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-12 23:12:59 +0900

    [Silver IV] Title: 스택 2, Time: 440 ms, Memory: 209580 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -5,28 +5,23 @@ n = int(input())
 stack = []
 
 for i in range(n):
-    cmd = input().split()
-
-    if cmd[0] == '1':
-        stack.append(cmd[1])
-
-    elif cmd[0] == '2':
+    a = input().split()
+    if a[0] == '1':
+        stack.append(a[1])
+    elif a[0] == '2':
         if len(stack) != 0:
             print(stack[-1])
-            del(stack[-1])
+            stack.pop()
         else:
             print(-1)
-
-    elif cmd[0] == '3':
+    elif a[0] == '3':
         print(len(stack))
-
-    elif cmd[0] == '4':
+    elif a[0] == '4':
         if len(stack) != 0:
             print(0)
         else:
             print(1)
-            
-    elif cmd[0] == '5':
+    elif a[0] == '5':
         if len(stack) != 0:
             print(stack[-1])
         else:
