2747a48afa1d219ebea438cf6bd7d01429502bb1
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-13 22:19:41 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,22 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 for i in range(n):
     stack = []
-    a = input()
+    a = input().strip()
     for j in a:
-        if j == '(':
+        if j == '(': #(가 나오면 스택에 저장
             stack.append(j)
-        elif j == ')':
-            if stack:
+        else: #)가 나온 후
+            if len(stack) != 0: #)이 나오면 스택에서 하나 제거
                 stack.pop()
-            else:
+            else: #스택이 비어있으면 NO
                 print('NO')
                 break
-    else:
-        if not stack:
-            print('YES')
-        else:
+    else: #break가 안됨
+        if len(stack) != 0:
             print('NO')
+        else:
+            print('YES')
