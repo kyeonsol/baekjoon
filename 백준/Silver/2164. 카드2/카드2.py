565ab06bd4d83025feda5bb867dad7ee0eff4112
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-17 14:50:29 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 130608 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,10 +3,19 @@ input = sys.stdin.readline
 from collections import deque
 
 n = int(input())
-lst = deque([i for i in range(1,n+1)])
+lst = list(range(1,n+1))
+queue = deque(lst) #카드 생성
+'''lst = deque([i for i in range(1,n+1)]) #deque 안에 많은 정보 넣지 말기!''' 
 
-while len(lst) > 1:
-    lst.popleft()
-    lst.rotate(-1)
-
-print(*lst)
+while 1:
+    if len(queue) == 1:
+        print(queue[0])
+        break
+    else:
+        queue.popleft()
+        if len(queue) == 1:
+            print(queue[0])
+            break
+        else:
+            queue.append(queue[0])
+            queue.popleft()
