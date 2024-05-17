43b3045ef4231b317de39388dc29a7f9e72c94f1
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-17 14:10:07 +0900

    [Silver IV] Title: 큐 2, Time: 936 ms, Memory: 254484 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -6,34 +6,29 @@ n = int(input())
 lst = deque([])
 
 for i in range(n):
-    a = input().split()
-
+    a = input().split() #index = 0~1
     if a[0] == 'push':
         lst.append(a[1])
-
     elif a[0] == 'pop':
-        if len(lst) != 0:
-            print(lst.popleft())
-        else:
+        if len(lst) == 0:
             print(-1)
-
+        else:
+            print(lst[0])
+            lst.popleft()
     elif a[0] == 'size':
         print(len(lst))
-
     elif a[0] == 'empty':
-        if len(lst) != 0:
-            print('0')
+        if len(lst) == 0:
+            print(1)
         else:
-            print('1')
-
+            print(0)
     elif a[0] == 'front':
-        if len(lst) != 0:
-            print(lst[0])
+        if len(lst) == 0:
+           print(-1)
         else:
-            print('-1')
-
+           print(lst[0])
     elif a[0] == 'back':
-        if len(lst) != 0:
-            print(lst[-1])
+        if len(lst) == 0:
+            print(-1)
         else:
-            print('-1')
+            print(lst[-1])
