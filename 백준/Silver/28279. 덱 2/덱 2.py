07d98f1d949ed04c8c6537b533e11b2b14f46463
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-20 01:30:31 +0900

    [Silver IV] Title: 덱 2, Time: 656 ms, Memory: 186892 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -3,46 +3,38 @@ input = sys.stdin.readline
 from collections import deque
 
 n = int(input())
-deque = deque()
+d = deque([])
 
 for i in range(n):
-    a = list(map(int,input().split()))
-
-    if a[0] == 1:
-        deque.appendleft(a[1])
-
-    elif a[0] == 2:
-        deque.append(a[1])
-
-    elif a[0] == 3:
-        if len(deque) != 0:
-            print(deque.popleft())
+    a = input().split()
+    if a[0] == '1':
+        d.appendleft(a[1])
+    elif a[0] == '2':
+        d.append(a[1])
+    elif a[0] == '3':
+        if len(d) != 0:
+            print(d.popleft())
         else:
             print(-1)
-
-    elif a[0] == 4:
-        if len(deque) != 0:
-            print(deque.pop())
+    elif a[0] == '4':
+        if len(d) != 0:
+            print(d.pop())
         else:
             print(-1)
-
-    elif a[0] == 5:
-        print(len(deque))
-
-    elif a[0] == 6:
-        if len(deque) != 0:
+    elif a[0] == '5':
+        print(len(d))
+    elif a[0] == '6':
+        if len(d) != 0:
             print(0)
         else:
             print(1)
-
-    elif a[0] == 7:
-        if len(deque) != 0:
-            print(deque[0])
+    elif a[0] == '7':
+        if len(d) != 0:
+            print(d[0])
         else:
             print(-1)
-
-    elif a[0] == 8:
-        if len(deque) != 0:
-            print(deque[-1])
+    elif a[0] == '8':
+        if len(d) != 0:
+            print(d[-1])
         else:
             print(-1)
