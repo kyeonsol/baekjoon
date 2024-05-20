71b2e43af10d25f2dff246cb330ae0448f767e8d
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-05-21 00:25:31 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 484 ms, Memory: 181512 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.py
@@ -7,17 +7,15 @@ score = []
 
 for i in range(n):
     a = list(map(int,input().split()))
-    if a == [0]:
-        if len(stack) != 0:
-            stack[-1][1] -= 1   
-            if stack[-1][1] == 0:
-                score.append(stack[-1][0])
-                stack.pop()
-    else:
+    if a == [0] and len(stack) != 0:
+        stack[-1][1] -= 1   
+
+    elif a != [0]:
         stack.append(a[1:])
         stack[-1][1] -= 1
-        if stack[-1][1] == 0:
-            score.append(stack[-1][0])
-            stack.pop()
+    
+    if len(stack) != 0 and stack[-1][1] == 0:
+        score.append(stack[-1][0])
+        stack.pop()
 
 print(sum(score))
