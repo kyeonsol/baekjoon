766284acbda00922f8162fdf0af3a97e854af30f
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-06-30 04:04:20 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 492 ms, Memory: 139500 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,17 +1,25 @@
 import sys
 input = sys.stdin.readline
-from collections import Counter
 
-n,m = map(int,input().split())
-lst = []
+#자주
+#길수록
+#사전 순
+
+n, m = map(int,input().split())
+lst = {}
 
 for i in range(n):
-    a = input().strip()
-    if len(a) >= m:
-        lst.append(a)
+    a = input().rstrip()
 
-result = Counter(lst).most_common()
-result.sort(key = lambda x : (-x[1], -len(x[0]), x[0]))
+    if len(a) < m: #m 이상의 단어만
+        continue
+    else:
+        if a in lst:
+            lst[a] += 1
+        else:
+            lst[a] = 1
 
-for i in result:
-    print(i[0])
+lst = sorted(lst.items(), key = lambda x : (-x[1], -len(x[0]), x[0]))
+
+for i in range(len(lst)):
+    print(lst[i][0])
