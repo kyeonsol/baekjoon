a7b92e2f027edfe54071fee475ac40f077718426
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-07-11 20:52:43 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 num = n
-count = 0
+cnt = 0
 
-while True:
-    a = num//10
-    b = num%10
-    c = (a+b)%10
+while 1:
+    a = num//10 #십의자리
+    b = num%10 #일의자리
+    c = (a+b)%10 #더해서 일의자리
     num = (b*10)+c
+    cnt += 1
     
-    count+=1
-    if(num==n):
+    if n == num:
         break
 
-print(count)
+print(cnt)
