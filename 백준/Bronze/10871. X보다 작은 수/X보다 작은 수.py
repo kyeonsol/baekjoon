b022708658bca977395f1b5e703676d9d236122e
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-07-11 20:50:22 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,5 +1,12 @@
-n,x= map(int,input().split())
-list= list(map(int,input().split()))
+import sys
+input = sys.stdin.readline
+
+n, x = map(int,input().split()) #x보다 작은 수
+lst = list(map(int,input().split()))
+res = []
+
 for i in range(n):
-    if list[i]<x:
-        print(list[i])
+    if lst[i] < x:
+        res.append(lst[i])
+
+print(*res)
