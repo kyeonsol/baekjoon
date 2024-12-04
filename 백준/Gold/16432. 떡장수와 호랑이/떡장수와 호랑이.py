bed299fea293ce671cccc96e51b6fd11dfb81f00
kyeonsol <129177554+kyeonsol@users.noreply.github.com>
2024-12-04 20:37:36 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 108 ms, Memory: 112684 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.py
@@ -1,29 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 def dfs(now,past,lst):
-    global visit
+    global visit #전역 변수 선언
 
-    if now == n:
+    if now == n: #다 돌았다면
         for i in range(n):
             print(lst[i])
-        exit(0)
+        exit(0) #이후 print(-1)이 실행되지 않도록 exit
     
-    for i in ddeok[now]:
+    for i in ddeok[now]: #
         if visit[now][i] == False:
             if i != past:
                 visit[now][i] = True
                 dfs((now+1), i, (lst+[i]))
 
 n = int(input())
-ddeok = []
-visit = [[False for i in range(10)] for j in range(n)]
-#떡 종류 10개
+ddeok = [] #떡 종류 정보 저장
+visit = [[False for i in range(10)] for j in range(n)] #떡 종류 10개
 
 for i in range(n):
-    ddeok.append(list(map(int,input().split()))[1:])
+    ddeok.append(list(map(int,input().split()))[1:]) #개수 제외하고 저장
 
-dfs(0,0,[])
+dfs(0,0,[]) #첫날은 past가 없음
 
-print(-1)
+print(-1) #떡을 줄 방법이 없다면
